2a975681536574bc7d6b87635b0423aa48e4c12b
Bagas Lodianto <bagas.lodianto@gdn-commerce.com>
2023-05-20 19:38:50 +0700

    6. Styling Services Section

--- src/components/ServicesSection.vue
+++ src/components/ServicesSection.vue
@@ -50,3 +50,62 @@ import diaphragm from '../assets/images/diaphragm.svg'
 import teamwork from '../assets/images/teamwork.svg'
 import home2 from '../assets/images/home2.png'
 </script>
+
+<style lang="scss" scoped>
+.services {
+  min-height: 90vh;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 5rem 10rem;
+  color: white;
+  .description {
+    flex: 1;
+    padding-right: 5rem;
+    h2 {
+      font-weight: lighter;
+      padding-bottom: 5rem;
+    }
+    p {
+      width: 70%;
+      padding: 2rem 0rem 4rem 0rem;
+    }
+
+    .cards {
+      display: flex;
+      flex-wrap: wrap;
+
+      .card {
+        flex-basis: 15rem;
+        .icon {
+          display: flex;
+          align-items: center;
+
+          h3 {
+            margin-left: 1rem;
+            background: white;
+            color: black;
+            padding: 1rem;
+          }
+        }
+      }
+    }
+
+    .title {
+      .hide {
+        overflow: hidden;
+      }
+    }
+  }
+
+  .image {
+    flex: 1;
+    overflow: hidden;
+    img {
+      width: 100%;
+      height: 80vh;
+      object-fit: cover;
+    }
+  }
+}
+</style>
