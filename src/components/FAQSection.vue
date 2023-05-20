0a5ba6f9e5fe70dce910a0b540cfb153c717e3f7
Bagas Lodianto <bagas.lodianto@gdn-commerce.com>
2023-05-20 15:32:21 +0700

    4. Faq Section

---
+++ src/components/FAQSection.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="faq">
+    <h2>Any Questions <span>FAQ</span></h2>
+    <!-- Question 1 -->
+    <div class="question">
+      <h4>How do I start?</h4>
+      <div class="answer">
+        <p>Lorem ipsum dolor sit amet.</p>
+        <p>Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.</p>
+      </div>
+    </div>
+    <!-- Question 2 -->
+    <div class="question">
+      <h4>Daily schedule</h4>
+      <div class="answer">
+        <p>Lorem ipsum dolor sit amet.</p>
+        <p>Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.</p>
+      </div>
+    </div>
+    <!-- Question 3 -->
+    <div class="question">
+      <h4>Different payment methods</h4>
+      <div class="answer">
+        <p>Lorem ipsum dolor sit amet.</p>
+        <p>Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.</p>
+      </div>
+    </div>
+    <!-- Question 4 -->
+    <div class="question">
+      <h4>What products do you offer?</h4>
+      <div class="answer">
+        <p>Lorem ipsum dolor sit amet.</p>
+        <p>Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup></script>
--- src/views/AboutUs.vue
+++ src/views/AboutUs.vue
@@ -2,10 +2,12 @@
   <main>
     <AboutSection />
     <ServicesSection />
+    <FAQSection />
   </main>
 </template>
 
 <script setup>
 import AboutSection from '../components/AboutSection.vue'
 import ServicesSection from '../components/ServicesSection.vue'
+import FAQSection from '../components/FAQSection.vue'
 </script>
