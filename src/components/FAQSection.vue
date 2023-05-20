882d0a3bba9fbb8ea9355a08612f32c6cd8cae00
Bagas Lodianto <bagas.lodianto@gdn-commerce.com>
2023-05-20 19:48:34 +0700

    7. Styling Faq Section

--- src/components/FAQSection.vue
+++ src/components/FAQSection.vue
@@ -8,6 +8,7 @@
         <p>Lorem ipsum dolor sit amet.</p>
         <p>Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.</p>
       </div>
+      <div class="faq-line"></div>
     </div>
     <!-- Question 2 -->
     <div class="question">
@@ -16,6 +17,7 @@
         <p>Lorem ipsum dolor sit amet.</p>
         <p>Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.</p>
       </div>
+      <div class="faq-line"></div>
     </div>
     <!-- Question 3 -->
     <div class="question">
@@ -24,6 +26,7 @@
         <p>Lorem ipsum dolor sit amet.</p>
         <p>Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.</p>
       </div>
+      <div class="faq-line"></div>
     </div>
     <!-- Question 4 -->
     <div class="question">
@@ -32,8 +35,43 @@
         <p>Lorem ipsum dolor sit amet.</p>
         <p>Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.</p>
       </div>
+      <div class="faq-line"></div>
     </div>
   </div>
 </template>
 
 <script setup></script>
+
+<style lang="scss" scoped>
+.faq {
+  min-height: 90vh;
+  display: block;
+  padding: 5rem 10rem;
+  color: white;
+  span {
+    display: block;
+  }
+  h2 {
+    padding-bottom: 2rem;
+    font-weight: lighter;
+  }
+  .faq-line {
+    background: #cccccc;
+    height: 0.2rem;
+    margin: 2rem 0rem;
+    width: 100%;
+  }
+
+  .question {
+    padding: 3rem 0rem;
+    cursor: pointer;
+  }
+
+  .answer {
+    padding: 2rem 0rem;
+    p {
+      padding: 1rem 0rem;
+    }
+  }
+}
+</style>
